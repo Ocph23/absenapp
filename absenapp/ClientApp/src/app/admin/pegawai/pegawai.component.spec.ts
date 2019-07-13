defb543ac8b524b6cf72b279f8441d082978e13d
kristt26 <kristt26@gmail.com>
2019-07-13 16:33:41 +0900

    #Fix Add Pegawai Not bnedahara

--- absenapp/ClientApp/src/app/admin/pegawai/pegawai.component.spec.ts
+++ absenapp/ClientApp/src/app/admin/pegawai/pegawai.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PegawaiComponent } from './pegawai.component',;
+import { PegawaiComponent } from './pegawai.component';
 
 describe('PegawaiComponent', () => {
   let component: PegawaiComponent;
--- absenapp/ClientApp/src/app/admin/pegawai/pegawai.component.ts
+++ absenapp/ClientApp/src/app/admin/pegawai/pegawai.component.ts
@@ -74,7 +74,7 @@ export class PegawaiComponent implements OnInit {
       'alamat': [null, Validators.required],
       'sex': [null, Validators.required],
       'jabatan': [null, Validators.required],
-      'bendahara': [null],
+      'bendahara': false,
     });
     this.modalService.open(content, {
       ariaLabelledBy: 'modal-basic-title', backdrop: 'static'
